f01fa226494abf3eab2e6d60c5273daa4c5db7a6
alisiddiqui03 <ali/siddiqui0324@gmail.com>
2021-12-19 17:45:59 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:filepicker/Helper/storage_helper.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -15,12 +19,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Storage storage = Storage();
   var results;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(title: Text("Firebase Storage"),),
+      appBar: AppBar(
+        title: Text("Firebase Storage"),
+      ),
       body: Column(
         children: [
           Center(
@@ -33,14 +40,17 @@ class _MyAppState extends State<MyApp> {
                 setState(() {
                   results = results;
                 });
+
                 if (results == null) {
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(
                       content: Text("No File Selected"),
                     ),
                   );
-                } 
-                
+                }
+                final pathname = results.files.single.path;
+                final filename = results.files.single.name;
+                storage.uploadfile(pathname, filename);
               },
               child: Text("Upload File"),
             ),
